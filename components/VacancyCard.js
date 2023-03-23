8c9f8d8ddfb3559f9889e1012033d2da3de86b7c
Faopaw <66549556+Faopaw@users.noreply.github.com>
2023-03-23 13:59:01 +0000

    css improvements

--- components/VacancyCard.js
+++ components/VacancyCard.js
@@ -4,10 +4,15 @@ function vacancyCard(props) {
   return (
     <>
       <Card
-        border="light"
+        // border="light"
         bg="light"
         text="dark"
-        style={{ width: "80%", justifySelf: "center" }}
+        style={{
+          width: "80%",
+          justifySelf: "center",
+          "--bs-border-opacity": "none",
+          borderLeft: "3px solid hsl(205, 69%, 50%)",
+        }}
       >
         <Card.Body>
           <Card.Title>{props.requireddata.fields.city["en-US"]}</Card.Title>
@@ -15,10 +20,12 @@ function vacancyCard(props) {
             {props.requireddata.fields.address["en-US"]}
           </Card.Subtitle>
           <Card.Text>
-            {props.requireddata.fields.requirements["en-US"]} - {props.requireddata.fields.extraDetails["en-US"]}
+            {props.requireddata.fields.requirements["en-US"]} -{" "}
+            {props.requireddata.fields.extraDetails["en-US"]}
           </Card.Text>
           <Card.Text>
-            {props.requireddata.fields.contactName["en-US"]} - {props.requireddata.fields.contactMobileNumber["en-US"]}
+            {props.requireddata.fields.contactName["en-US"]} -{" "}
+            {props.requireddata.fields.contactMobileNumber["en-US"]}
           </Card.Text>
           {/* <Card.Link href="#">Card Link</Card.Link>
           <Card.Link href="#">Another Link</Card.Link> */}
