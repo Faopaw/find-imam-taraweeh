9f2beca12c655238e89bdc62554a4920ff823883
Faopaw <66549556+Faopaw@users.noreply.github.com>
2023-03-28 11:09:31 +0100

    improved functionality

--- components/VacancyCard.js
+++ components/VacancyCard.js
@@ -1,6 +1,14 @@
 import Card from "react-bootstrap/Card";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 function vacancyCard(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <Card
@@ -20,17 +28,39 @@ function vacancyCard(props) {
             {props.requireddata.fields.address["en-US"]}
           </Card.Subtitle>
           <Card.Text>
-            {props.requireddata.fields.requirements["en-US"]} -{" "}
-            {props.requireddata.fields.extraDetails["en-US"]}
+            {props.requireddata.fields.requirements["en-US"]}
+            {/* {props.requireddata.fields.extraDetails["en-US"]} */}
           </Card.Text>
           <Card.Text>
             {props.requireddata.fields.contactName["en-US"]} -{" "}
             {props.requireddata.fields.contactMobileNumber["en-US"]}
           </Card.Text>
-          {/* <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link> */}
+          <Button variant="primary" onClick={handleShow}>
+            More Details
+          </Button>
         </Card.Body>
       </Card>
+
+      
+      {/* Modal */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>More Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Masjid Name: {props.requireddata.fields.masjid["en-US"]}
+          <br></br>
+          <br></br>
+
+          Extra Details: {props.requireddata.fields.extraDetails["en-US"]}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
