10176371db2c3bf096d5078f6781e2480c81f14c
Faopaw <66549556+Faopaw@users.noreply.github.com>
2023-04-13 13:07:59 +0100

    config changes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env : {
     PERSONAL_ACCESS_TOKEN: "CFPAT-fcueJ3JgHAfAxhshmTyf-xT-pBBA5hC3UPjqQ1ak7Wk",
-    CONTENTFUL_SPACE_ID : `jy2mhidk2k1l`
+    CONTENTFUL_SPACE_ID : `jy2mhidk2k1l`,
+    AUTH0_SECRET:'1dbd7dc5ac184e5161e30a272bce8e60417f227fa69774e3ebf008df43e4ecec'
   },
   images: {
     remotePatterns: [
