d15059b0831f831ead0cb99e43936d1fa3c897cb
Faopaw <66549556+Faopaw@users.noreply.github.com>
2023-04-13 13:22:34 +0100

    config correction

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
       "1dbd7dc5ac184e5161e30a272bce8e60417f227fa69774e3ebf008df43e4ecec",
     AUTH0_BASE_URL: "https://*.vercel.app/" || "http://localhost:3000",
     AUTH0_ISSUER_BASE_URL: "https://dev-au81z8esmiyhmtaq.uk.auth0.com",
-    AUTH0_CLIENT_ID: "NGp0Z4XlKlfukvbx5tNVFLre0mlNuSZF",
+    clientID: "NGp0Z4XlKlfukvbx5tNVFLre0mlNuSZF",
     AUTH0_CLIENT_SECRET:
       "01FhrqZMSd52McRG2dEQLwqcRaB3nraCGnlxI5j4KZL84oIP4P1LRnVHjwadaQqa",
   },
