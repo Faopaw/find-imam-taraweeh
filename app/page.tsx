2a705a1fb3479aaaa484856ab9f95c7fc2433325
Muhammed Patel <muhammed.patel@mindera.com>
2025-12-11 18:38:01 +0000

    feat: add loading handling when data is being fetched

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
+import { Suspense } from "react";
 import Hero from "../components/Hero";
 import Vacancies from "../components/Vacancies";
 import { getAllVacancies } from "@/lib/contentful/api";
+import Loading from "./loading";
 
 // Enable static generation with ISR - regenerate every hour
 export const revalidate = 3600;
 
-export default async function Home() {
-  const res = (await getAllVacancies());
+export async function VacanciesList() {
+  const res = await getAllVacancies();
+  return <Vacancies data={res} />;
+}
 
+export default async function Home() {
   return (
     <>
       <Hero
         title="All Vacancies"
         description="Use the information below to see all of the current Taraweeh vacancies nationwide"
       />
-      <Vacancies data={res} />
+      <Suspense fallback={<Loading />}>
+        <VacanciesList />
+      </Suspense>
     </>
   );
 }
